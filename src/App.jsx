99b496e0adcd4aedf1be7251cadb127361ceb91d
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-20 15:12:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    <section className="padding bg-pale-blue ">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding-x bg-black padding-t pb-8 ">
+      Footer
+    </section>
+  </main>
+);
+export default App;
