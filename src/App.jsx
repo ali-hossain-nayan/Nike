355fbe249287e3c86095ed3dee4aba7ab7e89d5d
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-20 17:03:48 +0600

    navbar-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
+import { CustomerReviews, Footer, Subscribe, SuperQuality, SpecialOffer, Hero, PopularProducts, Services } from "./components";
+import Nav from "./components/Nav";
 
 
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffers
+      <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue ">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="padding-x bg-black padding-t pb-8 ">
-      Footer
+      <Footer />
     </section>
   </main>
 );
---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality'
+import CustomerReviews from './CustomerReviews'
+import PopularProducts from './PopularProducts'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
