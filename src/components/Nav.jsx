10e08dc61cfc3646ca1b1e77707576eecb58fa3a
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-21 21:36:18 +0600

    navbar-updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import {headerLogo} from '../assets/images'
-import {hamburger} from '../assets/icons'
+import React, { useState } from 'react';
+import { headerLogo } from '../assets/images';
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
 
 const Nav = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
         <a href="/">
-          <img src={headerLogo} alt="header" width={130} height={29}/>
+          <img src={headerLogo} alt="header" width={130} height={29} />
         </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-
+        <ul className={`flex-1 flex justify-center items-center gap-16 ${isNavOpen ? '' : ''} max-lg:hidden`}>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className='font-monts errat leading-normal text-lg text-slate-gray'>
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
+        <div className="max-lg:block hidden" onClick={toggleNav}>
+          <img src={hamburger} alt="hamburg"
+            width={25}
+            height={25}
+          />
+        </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
