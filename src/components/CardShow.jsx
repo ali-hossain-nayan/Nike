b5d9b4130819ac72032a9e16c075f5d4e2c3e423
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-22 01:01:29 +0600

    hero-section-completed

---
+++ src/components/CardShow.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const CardShow = ({ imgUrl, changeShoeImg, bigShoeImg }) => {
+    const handleClick = () => {
+        if (bigShoeImg !== imgUrl.bigShoe) {
+            changeShoeImg(imgUrl.bigShoe)
+        }
+    }
+    return (
+        <div className={`border-2 rounded-xl ${bigShoeImg === imgUrl ? 'border-coral-red' : 'border-transparent'}
+     cursor-pointer max-sm:flex-1`} onClick={handleClick}>
+            <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+                <img src={imgUrl.thumbnail} alt="collection" width={127} height={104} className='object-contain' />
+            </div>
+
+        </div>
+    )
+}
+
+export default CardShow
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button'
+import { arrowRight } from '../assets/icons'
+import { shoes, statistics } from '../constants'
+import { bigShoe1 } from '../assets/images'
+import CardShow from './CardShow'
 
 const Hero = () => {
+  const [Img, setImg] = useState(bigShoe1)
   return (
-    <div>Test</div>
+    <section id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
+        <p className='text-xl text-violet-400'>Our Winter Collection</p>
+        <h1 className='mt-10 text-8xl max-sm:text[72px] font-bold '><span className=' xl:whitespace-nowra '>The New Arrival</span>
+          <br />
+          <span className='text-violet-500 p-2 inline-block mt-5 '>Nike</span>
+          Shoes
+        </h1>
+        <p className='p-4 text-lg mt-6 mb-6 sm:max-w-sm'>Discover stylish Nike arrivals,quality comfort and innovation for your active life</p>
+        <Button label="Shop now" iconUrl={arrowRight} />
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((item) => (
+            <div key={item.value}>
+              <p className='text-4xl font-bold'>{item.value}</p>
+              <p className='text-lg'>{item.label}</p>
+            </div>
+          ))}</div>
+      </div>
+      <div className='flex flex-1 relative justify-center items-center
+         max-xl:py-36 xl:min-h-screen bg-primary   bg-center '>
+        <img src={Img} alt="big-show-collection " width={600} height={500}
+          className='object-contain relative z-10'
+        />
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6 '>
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <CardShow imgUrl={shoe} changeShoeImg={(shoe) => setImg(shoe)} bigShoeImg={Img} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
