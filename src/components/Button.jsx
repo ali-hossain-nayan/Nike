ef55de073c3ee07fc548830184a70e5506f8ef22
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-22 23:06:56 +0600

    super-quality-section-completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,9 +5,9 @@ const Button = ({ label, iconUrl }) => {
     <button className=' flex justify-center items-center gap-2 px-8 py-5 
     font-montserrat text-lg text-white bg-violet-600  rounded-full hover:bg-blue-600'>
       {label}
-      <img src={iconUrl} alt="right arrow-icon"
+      {iconUrl && <img src={iconUrl} alt="right arrow-icon"
         className='ml-2 rounded-full w-6 h-6'
-      />
+      />}
     </button>
   )
 }
--- src/components/SuperQuality.jsx
+++ src/components/SuperQuality.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Button from './Button'
+import { shoe7 } from '../assets/images'
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id='about-us' className='flex justify-between items-center max-lg:flex-col gap-12 w-full max-container'>
+      <div className='flex flex-1 flex-col'>
+        <h1 className='mt-10 text-4xl max-sm:text[72px] font-bold '><span className=' xl:whitespace-nowra '>We Provide You</span>
+          <br />
+          <span className='text-violet-500 
+           inline-block mt-2 '>Super Quality</span><br />
+          Shoes
+        </h1>
+        <p className='p-3 text-lg mt-4 mb-4 sm:max-w-sm '>
+          Ensuring Premium comfort and style our crafted footwear is designed to elevate your experience, providing you the
+          unmatched quality...</p>
+        <div>
+
+          <Button label="View details" />
+        </div>
+
+      </div>
+      <div className='flex flex-1 justify-center items-center'>
+        <img src={shoe7} alt="shoe7" width={570} height={520} className='object-contain' />
+      </div>
+
+    </section>
   )
 }
 
