b333bd7bd180749c77d9b925bffe96d3d59f4601
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-23 13:23:01 +0600

    subscribe-section-completed

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Button from './Button'
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className='max-container' id='contact-us'>
+      <div className='flex flex-1 justify-center items-center mt-12 '>
+        <h2 className='text-4xl justify-center items-center'>Signup for <span className='text-violet-500'>Updates </span>& Newsletter</h2>
+      </div>
+      {/* lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full */}
+      <div className=' flex max-sm:justify-end items-center max-sm:w-full border rounded-full mt-8 p-2'>
+        <input type='text' placeholder='subscribe@nike.com' className='input' />
+        <Button label='Signup' />
+      </div>
+    </section>
   )
 }
 
