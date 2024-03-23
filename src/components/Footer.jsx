ee924e0a6908224fc4fa74370ba12f0f05294d88
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-23 13:40:56 +0600

    footer-completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className='max-container'>
+      <div className='flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
+        <div className='flex flex-col items-start'>
+          <a href='/'>
+            <img
+              src={footerLogo}
+              alt='logo'
+              width={150}
+              height={46}
+              className='m-0'
+            />
+          </a>
+          <p className='mt-6   text-white-400 sm:max-w-sm'>
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+          <div className='flex items-center gap-4 mt-10'>
+            {socialMedia.map((icon) => (
+              <div
+                className='flex justify-center items-center w-12 h-12 bg-white rounded-full'
+                key={icon.alt}
+              >
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
 
-export default Footer
+        <div className='flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap'>
+          {footerLinks.map((item) => (
+            <div key={item.title}>
+              <h4 className=' text-2xl  font-medium mb-6 text-white'>
+                {item.title}
+              </h4>
+              <ul>
+                {item.links.map((item) => (
+                  <li
+                    className='mt-2  text-white-400 '
+                    key={item.name}
+                  >
+                    <a href={item.link}>{item.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center'>
+        <div className='flex flex-1 justify-start items-center gap-2  cursor-pointer'>
+          <img
+            src={copyrightSign}
+            alt='copyright'
+            width={20}
+            height={20}
+            className='rounded-full flex justify-center items-center  '
+          />
+          {/* className="justify-center items-center flex flex-1" */}
+          <p >Copyright. All rights reserved Ali Hossain Nayan 2024.</p>
+        </div>
+        {/* <p className='font-montserrat cursor-pointer'>Terms & Conditions</p> */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
